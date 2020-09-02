10e63f39cdd0a611c59f6844564a63619a84db85
ChewyPaste <whmcheong@gmail.com>
2020-09-02 11:08:28 -0400

    rev1
    Added more prop-type coverage

--- src/NoteListMain/NoteListMain.js
+++ src/NoteListMain/NoteListMain.js
@@ -1,50 +1,64 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Note from '../Note/Note'
-import CircleButton from '../CircleButton/CircleButton'
-import ApiContext from '../ApiContext'
-import { getNotesForFolder } from '../notes-helpers'
-import './NoteListMain.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Note from "../Note/Note";
+import CircleButton from "../CircleButton/CircleButton";
+import ApiContext from "../ApiContext";
+import { getNotesForFolder } from "../notes-helpers";
+import "./NoteListMain.css";
+import PropTypes from "prop-types";
 
-export default class NoteListMain extends React.Component {
-  static defaultProps = {
-    match: {
-      params: {}
-    }
-  }
-  static contextType = ApiContext
+class NoteListMain extends React.Component {
+	static defaultProps = {
+		match: {
+			params: {},
+		},
+	};
+	static contextType = ApiContext;
 
-  render() {
-    const { folderId } = this.props.match.params
-    const { notes=[] } = this.context
-    const notesForFolder = getNotesForFolder(notes, folderId)
-    return (
-      <section className='NoteListMain'>
-        <ul>
-          {notesForFolder.map(note =>
-            <li key={note.id}>
-              <Note
-                id={note.id}
-                name={note.name}
-                modified={note.modified}
-              />
-            </li>
-          )}
-        </ul>
-        <div className='NoteListMain__button-container'>
-          <CircleButton
-            tag={Link}
-            to='/add-note'
-            type='button'
-            className='NoteListMain__add-note-button'
-          >
-            <FontAwesomeIcon icon='plus' />
-            <br />
-            Note
-          </CircleButton>
-        </div>
-      </section>
-    )
-  }
+	static propTypes = {
+		name: PropTypes.string.isRequired,
+		id: PropTypes.string.isRequired,
+		modified: PropTypes.string.isRequired,
+		onDeleteNote: PropTypes.func.isRequired,
+	};
+
+	render() {
+		const { folderId } = this.props.match.params;
+		const { notes = [] } = this.context;
+		const notesForFolder = getNotesForFolder(notes, folderId);
+		return (
+			<section className="NoteListMain">
+				<ul>
+					{notesForFolder.map((note) => (
+						<li key={note.id}>
+							<Note id={note.id} name={note.name} modified={note.modified} />
+						</li>
+					))}
+				</ul>
+				<div className="NoteListMain__button-container">
+					<CircleButton
+						tag={Link}
+						to="/add-note"
+						type="button"
+						className="NoteListMain__add-note-button"
+					>
+						<FontAwesomeIcon icon="plus" />
+						<br />
+						Note
+					</CircleButton>
+				</div>
+			</section>
+		);
+	}
 }
+
+NoteListMain.propTypes = {
+	match: PropTypes.shape({
+		params: PropTypes.shape({
+			folderId: PropTypes.string,
+		}),
+	}),
+};
+
+export default NoteListMain;
